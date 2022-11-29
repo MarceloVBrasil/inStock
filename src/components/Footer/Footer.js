5e4d3d17a4cf04c42400e833c6b5d65c49bef871
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-29 02:26:50 -0500

    add linkedin

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,23 @@ import './Footer.scss'
 export default function Footer() {
   return (
     <footer className='footer'>
-      <p>&copy; InStock Inc. All Rights Reserved.</p>
+      <p>&copy; <span onClick={jonah}>Jonah Hansen |</span> <span onClick={lucas}>Lucas Rodrigues |</span> <span onClick={adam}>Adam Pietrangelo |</span> <span onClick={marcelo}>Marcelo Brasil</span></p>
     </footer>
   )
+
+  function jonah() {
+    window.location.href = "https://www.linkedin.com/in/jonah-hansen-dev/"
+  }
+
+  function lucas() {
+    window.location.href = "https://www.linkedin.com/in/lucasoctavianorodrigues/"
+  }
+
+  function adam() {
+    window.location.href = "https://www.linkedin.com/in/adam-pietrangelo-dev/"
+  }
+
+  function marcelo() {
+    window.location.href = "https://www.linkedin.com/in/marcelo-vital-brasil/"
+  }
 }
